b3d7a5fcb7b1ba5b936f0ebe0ddad1c92d989257
Dung Paulus CTS <linhtam2001a@gmail.com>
2023-11-06 21:21:01 +0700

    add inventory,oder,oderdetail

--- Areas/Admin/AdminDTO/ProductViewDTO.cs
+++ Areas/Admin/AdminDTO/ProductViewDTO.cs
@@ -6,9 +6,7 @@
         public string?MaSanPham { get; set; }
         public string? TenSanPham { get; set; }
         public string? HangTen { get; set; } // Tên của Brand
-        public string? LoaiTen { get; set; } // Tên của Category
         public decimal Gia { get; set; }
-        public string? ThongTinSanPham { get; set; }
         public string? Image { get; set; }
 
         internal ProductViewDTO Select(Func<object, ProductViewDTO> value)
